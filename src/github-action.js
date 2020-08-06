a39f9e9b5a0259681829423404c2d671907d2d25
Restyled.io <commits@restyled.io>
2020-08-06 14:58:08 +0000

    Restyled by prettier

--- src/github-action.js
+++ src/github-action.js
@@ -1,11 +1,12 @@
 const core = require('@actions/core');
-const {exec} = require('./utils');
+const { exec } = require('./utils');
 
 const setup_cml = async () => {
   const version = core.getInput('version');
   core.info(`Intalling CML version ${version}`);
   await exec(
-      `npm i -g @dvcorg/cml${version !== 'latest' ? `@${version}` : ''}`);
+    `npm i -g @dvcorg/cml${version !== 'latest' ? `@${version}` : ''}`
+  );
 };
 
 const setup_dvc = async () => {
@@ -15,7 +16,8 @@ const setup_dvc = async () => {
   if (install === 'true') {
     core.info(`Intalling DVC version ${version}`);
     await exec(
-        `pip install dvc[all]${version !== 'latest' ? `==${version}` : ''}`);
+      `pip install dvc[all]${version !== 'latest' ? `==${version}` : ''}`
+    );
   }
 };
 
